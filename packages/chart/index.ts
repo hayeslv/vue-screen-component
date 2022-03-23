deed1ca72c2fe4d56de7c6b8289031fa2c8a0b3d
lvhaizhou <417703682@qq.com>
2022-03-23 10:43:18 +0800

    修改type

--- packages/chart/index.ts
+++ packages/chart/index.ts
@@ -1,12 +1,11 @@
 import type { App } from "vue";
-import hayChart from "./src/hayChart.vue";
-
-type SFCWithInstall<T> = T & { install(app: App): void }; // vue 安装
+import HayChart from "./src/hayChart.vue";
+import type { SFCWithInstall } from "../types";
 
 // 安装
-hayChart.install = (app: App): void => {
-  app.component(hayChart.name, hayChart);
+HayChart.install = (app: App): void => {
+  app.component(HayChart.name, HayChart);
 };
 
-const InHayChart: SFCWithInstall<typeof hayChart> = hayChart; // 增加类型
+const InHayChart: SFCWithInstall<typeof HayChart> = HayChart; // 增加类型
 export default InHayChart;
