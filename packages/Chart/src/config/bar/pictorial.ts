153ce921fabe222c6642d592da71927572b5ddfd
linzx <linzx@citycloud.com.cn>
2022-04-01 14:54:49 +0800

    fix(Chart): 去掉无用参数

--- packages/Chart/src/config/bar/pictorial.ts
+++ packages/Chart/src/config/bar/pictorial.ts
@@ -3,9 +3,9 @@ import { defaultBackgroundColor } from "../../common";
 import { getPictorialBarSeriesItem } from "../../defaultPictorialBarOptions";
 import { getDefaultTooltip } from "../../defaultBarOptions";
 import { getDefaultXAxis, getDefaultYAxis, getLegendBottom, getGridBottom } from "../../defaultOptions";
-import type { BarDataType, OptionConfig } from "../../types";
+import type { BarDataType } from "../../types";
 
-export const getOption = (dataList: Array<BarDataType>, config: OptionConfig = {}) => {
+export const getOption = (dataList: Array<BarDataType>) => {
   const options: EChartsOption = {
     grid: getGridBottom(),
     legend: getLegendBottom("bar"),
@@ -13,7 +13,7 @@ export const getOption = (dataList: Array<BarDataType>, config: OptionConfig = {
     xAxis: getDefaultXAxis(),
     yAxis: getDefaultYAxis(),
     backgroundColor: defaultBackgroundColor,
-    series: getPictorialBarSeriesItem(dataList, "vertical", config),
+    series: getPictorialBarSeriesItem(dataList, "vertical"),
   };
   return options;
 };
--- packages/Chart/src/config/bar/pictorial_horizon.ts
+++ packages/Chart/src/config/bar/pictorial_horizon.ts
@@ -3,9 +3,9 @@ import { defaultBackgroundColor } from "../../common";
 import { getPictorialBarSeriesItem } from "../../defaultPictorialBarOptions";
 import { getDefaultTooltip } from "../../defaultBarOptions";
 import { getDefaultXAxis, getDefaultYAxis, getLegendBottom, getGridBottom } from "../../defaultOptions";
-import type { BarDataType, OptionConfig } from "../../types";
+import type { BarDataType } from "../../types";
 
-export const getOption = (dataList: Array<BarDataType>, config: OptionConfig = {}) => {
+export const getOption = (dataList: Array<BarDataType>) => {
   const options: EChartsOption = {
     grid: getGridBottom(),
     legend: getLegendBottom("bar"),
@@ -13,7 +13,7 @@ export const getOption = (dataList: Array<BarDataType>, config: OptionConfig = {
     xAxis: getDefaultXAxis("value"),
     yAxis: getDefaultYAxis("category"),
     backgroundColor: defaultBackgroundColor,
-    series: getPictorialBarSeriesItem(dataList, "horizon", config),
+    series: getPictorialBarSeriesItem(dataList, "horizon"),
   };
   return options;
 };
